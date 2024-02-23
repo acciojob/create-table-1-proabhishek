6dc10b7855eb61ae5d998919e0407058565fb6b1
Abhishek Shankar Choudhary <28715657+proabhishek@users.noreply.github.com>
2024-02-23 22:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function insert_Row() {
     //Write your code here
     let table = document.getElementById('sampleTable');
-    let btn = document.querySelector('input'); 
+    
 
 
-    btn.addEventListener('click', addRow) 
+    // btn.addEventListener('click', addRow) 
 
-    function addRow() {
+    
+        console.log("I am executed")
         let row = table.insertRow(0);
         row.insertCell(0).innerHTML = "New Cell1";
         row.insertCell(1).innerHTML = "New Cell2";
-    }
+    
 
 
 }
