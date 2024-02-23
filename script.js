51c591798c7cd7641e30385e215efb170201fd37
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2024-02-23 22:29:17 +0530

    added table row

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
-  
-  
+    let table = document.getElementById('sampleTable');
+    let btn = document.querySelector('button'); 
+
+
+    btn.addEventListener('click', addRow) 
+
+    function addRow() {
+        let row = table.insertRow(0);
+        row.insertCell(0).innerHTML = "New Cell 1";
+        row.insertCell(1).innerHTML = "New Cell 2";
+    }
+
+
 }
